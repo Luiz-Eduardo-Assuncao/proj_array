0a351a19ef6f875d4de14d33d447f7ff14b29db6
Luiz <luiz_e_assuncao@estudante.sc.senai.br>
2023-03-13 09:24:01 -0300

    atualizado

---
+++ js.js
@@ -0,0 +1,14 @@
+let res=document.getElementById('res')
+let nome_cidade = []
+
+function inserir(){
+    let cidade = document.getElementById('cidade').value
+    nome_cidade.push(cidade)
+    console.log(nome_cidade)
+}
+
+function excluir(){
+    let cidade = document.getElementById('cidade').value
+    nome_cidade.pop(cidade)
+    console.log(nome_cidade)
+}
